a6a9f91d525832d378cf1d24cad6d10fdbd7a97a
filecc <fil.gia@live.it>
2023-05-30 14:00:59 +0200

    fixed an error with queryselector not working when in hompage and index

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,22 +5,27 @@ import.meta.glob([
     '../img/**'
 ])
 
-const deleteButton = document.querySelector('button.deleteButton')
-const confirmButton = document.querySelector('button.confirmDelete')
-const undoDelete = document.querySelector('button.undoDelete')
 
-const handleClick = function (event)
-{
-    event.preventDefault();
-    deleteButton.disabled = true
-    confirmButton.classList.remove('d-none')
-    undoDelete.classList.remove('d-none')
-    undoDelete.addEventListener('click', (e) => {
-        e.preventDefault()
-        deleteButton.disabled = false
-        confirmButton.classList.add('d-none')
-        undoDelete.classList.add('d-none')
-    })
+if (window.location.href.includes('comics/')){
+    const deleteButton = document.querySelector('button.deleteButton')
+    const confirmButton = document.querySelector('button.confirmDelete')
+    const undoDelete = document.querySelector('button.undoDelete')
+
+    const handleClick = function (event)
+    {
+        event.preventDefault();
+        deleteButton.disabled = true
+        confirmButton.classList.remove('d-none')
+        undoDelete.classList.remove('d-none')
+        undoDelete.addEventListener('click', (e) => {
+            e.preventDefault()
+            deleteButton.disabled = false
+            confirmButton.classList.add('d-none')
+            undoDelete.classList.add('d-none')
+        })
+    }
+
+    deleteButton.addEventListener('click', handleClick)
 }
 
-deleteButton.addEventListener('click', handleClick)
+
